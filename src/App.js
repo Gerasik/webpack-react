93a35e174811153922206caedbaf3190655accd9
Gerasik <gerasik1992@gmail.com>
2020-02-27 10:40:34 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div>
+            <h1>Hello world</h1>
+            <p>Lorem text</p>
+        </div>
+    ) 
+}
+
+export default App;

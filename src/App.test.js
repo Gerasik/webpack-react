af0dee4618fab9d306397ecbc351dd3d550a65be
Gerasik <gerasik1992@gmail.com>
2020-03-05 15:42:48 +0300

    add editorconfig and eslint

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import App from './App';
 import renderer from 'react-test-renderer';
+import App from './App';
 
 it('renders correctly', () => {
-    const tree = renderer
-      .create(<App />)
-      .toJSON();
-    expect(tree).toMatchSnapshot();
-  });
+  const tree = renderer
+    .create(<App />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});

aff67c1d9f2fd3d811fa4cadf90731c0eed77085
Gerasik <gerasik1992@gmail.com>
2020-03-13 00:39:42 +0300

    add reselect js

--- src/components/HomePage/index.tsx
+++ src/components/HomePage/index.tsx
@@ -1,9 +1,16 @@
 import { connect } from 'react-redux';
 import { addCounterAction, decrementAction, incrementAction } from './actions'
 import HomePage from './HomePage'
+import { ICounter } from './types';
+import { createSelector } from "reselect";
 
-const mapStateToProps = (state) => ({
-  counters: state.toJS().counters,
+const countersSelector = createSelector(
+  [(state: { toJS: () => { counters: ICounter; }; }) => state.toJS().counters],
+  (counters) => counters
+)
+
+const mapStateToProps = (state: { toJS: () => { counters: ICounter; }; }) => ({
+  counters: countersSelector(state)
 });
 
 const mapDispatchToProps = (dispatch: (arg0: { type: string; id?: number; value?: number}) => void) => ({

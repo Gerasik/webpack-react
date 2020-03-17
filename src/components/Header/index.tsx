0a251583013cbebb7d7de58fd8d552d0fd0913ef
Gerasik <gerasik1992@gmail.com>
2020-03-17 15:33:51 +0300

    fix: remove Extra line

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,21 +1,26 @@
-import React from 'react';
+import React, { ReactElement } from 'react';
 import { NavLink } from 'react-router-dom';
 import './style.scss';
 
-
-export default function Header() {
+function Header(): ReactElement {
   return (
     <header>
       <nav>
         <ul>
           <li>
-            <NavLink to="/" exact activeClassName="active">Home</NavLink>
+            <NavLink to="/" exact activeClassName="active">
+              Home
+            </NavLink>
           </li>
           <li>
-            <NavLink to="/about" activeClassName="active">About</NavLink>
+            <NavLink to="/about" activeClassName="active">
+              About
+            </NavLink>
           </li>
         </ul>
       </nav>
     </header>
   );
 }
+
+export default Header;
